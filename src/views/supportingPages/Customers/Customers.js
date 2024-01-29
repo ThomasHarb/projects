d5279fa37fee1891687394d535afe8019acac275
ThomasHarb <harbonnier.th@gmail.com>
2024-01-29 17:08:30 +0100

    Improve card and add content to projects 2

--- src/views/supportingPages/Customers/Customers.js
+++ src/views/supportingPages/Customers/Customers.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Container from '../../../common/Container';
-import { Stories } from './components';
+import React from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Container from "../../../common/Container";
+import { Stories } from "./components";
 
-const Customers = ({ themeMode = 'dark' }) => {
+const Customers = ({ themeMode = "dark" }) => {
   const theme = useTheme();
   return (
     <Box>
       <Container>
         <Stories themeMode={themeMode} />
       </Container>
-      <Box bgcolor={theme.palette.alternate.main}>
-      </Box>
+      <Box bgcolor={theme.palette.alternate.main}></Box>
     </Box>
   );
 };
--- src/views/supportingPages/Customers/components/Stories/Stories.js
+++ src/views/supportingPages/Customers/components/Stories/Stories.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 /*import { useTheme } from '@mui/material/styles';*/
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-import CardMedia from '@mui/material/CardMedia';
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import { LuBrainCircuit } from "react-icons/lu";
+import CardActions from "@mui/material/CardActions";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Avatar from "@mui/material/Avatar";
+import { FaChartArea } from "react-icons/fa";
 
 const Stories = () => {
   /*const theme = useTheme();*/
@@ -18,87 +20,199 @@ const Stories = () => {
       <Box marginBottom={4}>
         <Typography
           sx={{
-            textTransform: 'uppercase',
-            fontWeight: 'medium',
+            textTransform: "uppercase",
+            fontWeight: "medium",
           }}
           gutterBottom
-          color={'secondary'}
-          align={'center'}
+          color={"secondary"}
+          align={"center"}
         >
           Projects
         </Typography>
         <Box
           component={Typography}
           fontWeight={700}
-          variant={'h3'}
-          align={'center'}
+          variant={"h3"}
+          align={"center"}
         >
           A journey of turning data into actionable intelligence
           <br />
-          Explore the fusion of Python development, data science and data expertise
+          Explore the fusion of Python development, data science and data
+          expertise
         </Box>
       </Box>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardContent>
-              <Typography 
-                gutterBottom variant="h5" 
-                component="div" 
-                align='center'
+      <Grid container spacing={2} justifyContent="center" alignItems="stretch">
+        <Grid item xs={12} sm={6} md={4} style={{ display: "flex" }}>
+          <Card
+            sx={{
+              maxWidth: 343,
+              borderRadius: "20px",
+              boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+              transition: "0.3s",
+            }}
+          >
+            <Box sx={{ minWidth: 256 }}>
+              <Box
+                sx={{
+                  padding: `4px 24px 0`,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
               >
-                Data Analysis of Netflix movies
+                <Avatar
+                  alt={"brand logo"}
+                  sx={(theme) => ({
+                    width: 48,
+                    height: 48,
+                    transform: "translateY(50%)",
+                    "& > img": {
+                      margin: 0,
+                      backgroundColor: "common.white",
+                    },
+                    [theme.breakpoints.up("sm")]: {
+                      width: 60,
+                      height: 60,
+                    },
+                  })}
+                >
+                  <FaChartArea />
+                </Avatar>
+                <Typography
+                  sx={{
+                    textTransform: "uppercase",
+                    fontSize: 14,
+                    color: "grey.500",
+                    letterSpacing: "1px",
+                  }}
+                >
+                  7 min read
+                </Typography>
+              </Box>
+              <Box
+                component="hr"
+                sx={(theme) => ({
+                  backgroundColor: "grey.200",
+                  marginBottom: `${24 - 1}px`, // minus 1 due to divider height
+                  [theme.breakpoints.up("sm")]: {
+                    marginBottom: `${30 - 1}px`, // minus 1 due to divider height
+                  },
+                })}
+              />
+            </Box>
+            <CardContent>
+              <Typography variant="h5" gutterBottom>
+                Data Analysis of all movies in my Cloud
               </Typography>
-              <Typography 
-                variant="body2" 
+              <Typography
+                variant="body2"
+                component="div"
                 color="text.secondary"
-                align='center'
               >
-                Analysis or all movie on Netflix. The dataset has been downloaded from Kaggle. The data will be processed in Python then integrated in a MySQL database. The data will be then display in PowerBI.
+                Analysis or all movie on Netflix. The dataset has been
+                downloaded from Kaggle. The data will be processed in Python
+                then integrated in a MySQL database. The data will be then
+                display in PowerBI.
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                <br />
+                Languages and softwares used
+              </Typography>
+              <Typography variant="body2">
+                Python - MySQL - PowerBi - Tableau - excel
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size='small' href='/'>Learn more</Button>
+              <Button size="small" href="/">
+                Learn more
+              </Button>
             </CardActions>
           </Card>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140}}
-              image=""
-              title="Project2"
-            />
+        <Grid item xs={12} sm={6} md={4} style={{ display: "flex" }}>
+          <Card
+            sx={{
+              maxWidth: 343,
+              borderRadius: "20px",
+              boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+              transition: "0.3s",
+            }}
+          >
+            <Box sx={{ minWidth: 256 }}>
+              <Box
+                sx={{
+                  padding: `4px 24px 0`,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Avatar
+                  alt={"brand logo"}
+                  sx={(theme) => ({
+                    width: 48,
+                    height: 48,
+                    transform: "translateY(50%)",
+                    "& > img": {
+                      margin: 0,
+                      backgroundColor: "common.white",
+                    },
+                    [theme.breakpoints.up("sm")]: {
+                      width: 60,
+                      height: 60,
+                    },
+                  })}
+                >
+                  <LuBrainCircuit />
+                </Avatar>
+                <Typography
+                  sx={{
+                    textTransform: "uppercase",
+                    fontSize: 14,
+                    color: "grey.500",
+                    letterSpacing: "1px",
+                  }}
+                >
+                  20 min read
+                </Typography>
+              </Box>
+              <Box
+                component="hr"
+                sx={(theme) => ({
+                  backgroundColor: "grey.200",
+                  marginBottom: `${24 - 1}px`, // minus 1 due to divider height
+                  [theme.breakpoints.up("sm")]: {
+                    marginBottom: `${30 - 1}px`, // minus 1 due to divider height
+                  },
+                })}
+              />
+            </Box>
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Project 2
+              <Typography variant="h5" gutterBottom>
+                Store management system and Data science
               </Typography>
-              <Typography variant="body2" color="text.secondary">
-                The project 2 is blabla
+              <Typography
+                variant="body2"
+                component="div"
+                color="text.secondary"
+                textAlign="justify"
+              >
+                The purpose of this application is to help mangement of food. A
+                python script reads the receipt or bar-codes from food element,
+                and store them in MySQL Database. Then another Python script
+                helps me to find recipe according to all elements store in the
+                databases.
               </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                <br />
+                Languages and softwares used
+              </Typography>
+              <Typography variant="body2">Python - MySQL</Typography>
             </CardContent>
             <CardActions>
-              <Button size='small'>Learn more</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              sx={{ height: 140}}
-              image=""
-              title="Project3"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Project 3
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                The project 3 is blabla
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size='small'>Learn more</Button>
+              <Button size="small" href="/">
+                Learn more
+              </Button>
             </CardActions>
           </Card>
         </Grid>
